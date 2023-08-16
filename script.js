a04cdd545cd1245506b40ae8090601ffc2f8a376
dyrhave <126078101+dyrhave@users.noreply.github.com>
2023-08-16 02:40:36 +0200

    make script function with ui

--- script.js
+++ script.js
@@ -9,18 +9,31 @@ function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice];
 }
-// generates random choice for computer and gets it
 
-function playRound(playerSelection, computerSelection) {
+let playerScore = 0;
+let computerScore = 0;
+
+function updateScore(result) {
+    if (result.includes("win")) {
+        playerScore++;
+    } else if (result.includes("lose")) {
+        computerScore++;
+    }
+
+    document.getElementById("player-score").textContent = `Player: ${playerScore}`
+    document.getElementById("computer-score").textContent = `Computer: ${computerScore}`
+
+    if (playerScore === 5 || computerScore === 5) {
+        displayWinner();
+    }
+}
+
+function determineWinner(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    if (!choices.includes(playerSelection)) {
-        return "Invalid choice. Please choose rock, paper, or scissors";
-    }
-
     if (playerSelection === computerSelection) {
-        return "It's a tie!"
+        return "It's a tie!";
     }
 
     const winCondition = {
@@ -32,47 +45,45 @@ function playRound(playerSelection, computerSelection) {
     if (winCondition[playerSelection] === computerSelection) {
         return `You win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`; 
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
 
-    while (playerScore < 5 && computerScore < 5) {
-        const playerSelection = prompt(`Round ${round}: Enter your choice(rock, paper, or scissors)`);
-        const computerSelection = getComputerChoice();
-
-        console.log(`Player: ${playerSelection}`);
-        console.log(`Computer ${computerSelection}`);
-
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
-
-        if (result.includes("win")) {
-            playerScore++
-        } else if (result.includes("lose")) {
-            computerScore++
-        }
-
-        console.log(`Current score - Player: ${playerScore}, Computer: ${computerScore}`);
+function displayWinner() {
+    let finalResult;
+    if (playerScore > computerScore) {
+        finalResult = "Congratulation! You win!";
+    } else {
+        finalResult = "Sorry! You lose!"
     }
-    console.log(`Final score - Player: ${playerScore}, Computer: ${computerScore}`);
 
-    function getGameResult() {
-        if (playerScore > computerScore) {
-            return "Congratulations you win!"
-        } else if (playerScore < computerScore) {
-            return "Sorry you lose!"
-        } else {
-            return "Game ends in a draw"
-        }
-    }
-    const gameResult = getGameResult(playerScore, computerScore);
-    console.log(gameResult);
+    document.getElementById("round-result").textContent = finalResult;
+
+    choiceButtons.forEach(button => {
+        button.disabled = true;
+    });
 }
 
-game(); 
+const choiceButtons = document.querySelectorAll(".choice");
+choiceButtons.forEach(button => {
+    button.addEventListener("click", () => playRound(button.id));
+});
 
-// rewrite win condition to first to 5
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+
+    document.getElementById("player-choice").textContent = `Player: ${playerSelection}`;
+    document.getElementById("computer-choice").textContent = `Computer: ${computerSelection}`;
+
+    const result = determineWinner(playerSelection, computerSelection);
+
+    document.getElementById("round-result").textContent = result;
+
+    updateScore(result)
+
+    if (playerScore === 5 || computerScore === 5) {
+        displayWinner();
+    }
+}
+
